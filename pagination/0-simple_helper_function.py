3ffa67f06e9cc043cc2ea14699f8a3f13290134d
markjoehayes <markjoehayes@proton.me>
2025-12-03 08:54:06 -0400

    added a check for invalid input

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -6,6 +6,9 @@ from typing import Tuple
 
 def index_range(page: int, page_size: int) -> tuple[int, int]:
     """Calculate the start and end index for pagnination and return a tuple"""
+    if page < 1 or page_size < 1:
+        pass
+    
     #calculate the start index
     start_index = (page - 1) * page_size
     # calculate the end index
