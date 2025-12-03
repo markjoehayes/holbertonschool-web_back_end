25e106f03784976b775f3600db947f2ac99deb09
markjoehayes <markjoehayes@proton.me>
2025-12-03 08:46:40 -0400

    remove type hints from parameters

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """Helper function to return start and end indixes of pagination"""
 
-from typing import Tuple
 
-
-def index_range(page: int, page_size: int) -> tuple:
+def index_range(page, page_size) -> tuple:
     """Calculate the start and end index for pagnination and return a tuple"""
     #calculate the start index
     start_index = (page - 1) * page_size
