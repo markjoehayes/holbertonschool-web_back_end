eb8776d812c5fc8deae6fadf16010b09a680d61a
markjoehayes <markjoehayes@proton.me>
2025-12-03 08:40:55 -0400

    added weird docs

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Helper function to return start and end indixes of pagination"""
 
 def index_range(page: int, page_size: int) -> tuple:
     """Calculate the start and end index for pagnination and return a tuple"""
