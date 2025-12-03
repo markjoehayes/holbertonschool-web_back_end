d9d13cce5a5940666d75e66e85ad10352771c378
markjoehayes <markjoehayes@proton.me>
2025-12-03 11:21:14 -0400

    added a space after #

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -9,7 +9,7 @@ def index_range(page: int, page_size: int) -> tuple[int, int]:
 
     if page < 1 or page_size < 1:
         pass
-    #calculate the start index
+    # calculate the start index
     start_index = (page - 1) * page_size
     # calculate the end index
     end_index = page * page_size
