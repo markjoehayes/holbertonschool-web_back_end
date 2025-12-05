447f3a41c2384615e22d2f79e023c98e6b1e33d2
markjoehayes <markjoehayes@proton.me>
2025-12-05 08:37:03 -0400

    1st

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import csv
+import math
+from typing import Tuple, List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(sef):
+        self.__dataset = None
+
+    def dataset(self) -> List[list]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+        return self.__dataset
+
+    def get_page(slf, page: int = 1, page_size: int = 10) -> List[list]:
+        """Get corrosponding page"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        pagination_range: Tuple = self.index_range(page, page_size)
+        data: List = self.dataset()
+
+        return data[pagination_range[0] : pagination_range[1]]
+
+
+    def index_range(page: int, page_size: int) -> tuple[int, int]:
+        """Calculate the start and end index for pagnination and return a tuple"""
+
+        if page < 1 or page_size < 1:
+            pass
+        # calculate the start index
+        start_index = (page - 1) * page_size
+        # calculate the end index
+        end_index = page * page_size
+        return (start_index, end_index)
+
