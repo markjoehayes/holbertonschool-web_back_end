1e3ef70e9a0e41c38e8b89c5737a981e55e86c29
markjoehayes <markjoehayes@proton.me>
2025-12-05 08:54:13 -0400

    changed order!

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -8,7 +8,7 @@ class Server:
     """Server class to paginate a database of popular baby names."""
     DATA_FILE = "Popular_Baby_Names.csv"
 
-    def __init__(sef):
+    def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[list]:
@@ -20,16 +20,6 @@ class Server:
             self.__dataset = dataset[1:]
         return self.__dataset
 
-    def get_page(slf, page: int = 1, page_size: int = 10) -> List[list]:
-        """Get corrosponding page"""
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
-
-        self.dataset()
-        correct_index = index_range(page, page_size)
-        if correct_index[0] > len(self.__dataset):
-            return []
-        return self.__dataset[correct_index[0]:correct_index[1]]
 
     def index_range(page: int, page_size: int) -> tuple[int, int]:
         """Calculate the start and end index for pagnination and return a tuple"""
@@ -41,4 +31,15 @@ class Server:
         # calculate the end index
         end_index = page * page_size
         return (start_index, end_index)
+
+    def get_page(slf, page: int = 1, page_size: int = 10) -> List[list]:
+        """Get corrosponding page"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        self.dataset()
+        correct_index = index_range(page, page_size)
+        if correct_index[0] > len(self.__dataset):
+            return []
+        return self.__dataset[correct_index[0]:correct_index[1]]
 
