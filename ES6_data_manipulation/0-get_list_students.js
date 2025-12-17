b55bf8f49e241a82d2fde641c061e27f11f268ce
markjoehayes <markjoehayes@proton.me>
2025-12-17 08:22:43 -0400

    1st commit

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+    return [
+        { firstname: 'Guillaume', id: 1, location: 'San Francisco' },
+        { firstname: 'James', id: 2, location: 'Columbia' },
+        { firstname: 'Serena', id: 5, location: 'San Francisco' },
+        ];
+}
