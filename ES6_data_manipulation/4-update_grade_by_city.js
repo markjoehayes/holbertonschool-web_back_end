f3eda81ad58c584a903810bb0467bd6cc44ea0e7
markjoehayes <markjoehayes@proton.me>
2025-12-19 08:26:26 -0400

    final

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,17 +2,17 @@ export default function updateStudentGradeByCity(studentList, city, newGrades) {
     if (!(studentList instanceof Array)) {
         return [];
     }
-    function addGrade(student) {
-        newGrades.forEach((person) => {
-            if (person.studentID === student.id){
-                student.grade = person.grade;
-            }
-        });
-        if (student.grade === undefined) {
-            student.grade = 'N/A';
-        }
-        return student;
-    }
-        const filteredStudentList = studentList.filter((students) => students.location === city);
-        return filteredStudentList.map(addGrade);
+    
+    const filteredStudentList = studentList.filter((student) => student.location === city);
+    
+    return filteredStudentList.map((student) => {
+        // Find the grade for this student
+        const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+        
+        // Create a new object with the grade added
+        return {
+            ...student,
+            grade: gradeObj ? gradeObj.grade : 'N/A'
+        };
+    });
 }
