c58996a0464551ccc2c3ecfc9902b2b7b323b899
markjoehayes <markjoehayes@proton.me>
2026-01-06 17:47:10 -0400

    rmove sem-colon after func def

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,13 @@
-console.log('Welcome to Holberton School, what is your name?');
-// await input
-process.stdin.resume();
-// once input is entered
-process.stdin.on('readable', () => {
-    const name = process.stdin.read();
-    process.stdout.write(`Your name is: ${name}`);
-    process.stdout.write('This important software is now closing\n');
-    process.exit();
-        
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', (data) => {
+  const name = data.replace(/\r?\n$/, ''); // remove newline
+  process.stdout.write(`Your name is: ${name}\n`);
+
+process.stdin.on('end', () => {
+  // Only print closing message for piped input
+  process.stdout.write('This important software is now closing\n');
 });
+
