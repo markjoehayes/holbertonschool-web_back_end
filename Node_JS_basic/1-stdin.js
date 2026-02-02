502080d4b18bb330d9dd8220022e79e6e7951fcc
markjoehayes <markjoehayes@proton.me>
2026-02-01 22:24:04 -0400

    move close statement to data andler

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,8 +5,6 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', (data) => {
   const name = data.replace(/\r?\n$/, ''); // remove newline
   process.stdout.write(`Your name is: ${name}\n`);
-
-process.stdin.on('end', () => {
-  // Only print closing message for piped input
-  process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');  
+  process.exit(0);
 });
