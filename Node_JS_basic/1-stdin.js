198d270c69c78b1720015dc30ad5008de077ab33
markjoehayes <markjoehayes@proton.me>
2026-02-01 22:32:20 -0400

    program ends

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,9 +2,20 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
 
+// Check if input is coming from a terminal (interactive) or a pipe
+const isTTY = process.stdin.isTTY;
+
 process.stdin.on('data', (data) => {
   const name = data.replace(/\r?\n$/, ''); // remove newline
   process.stdout.write(`Your name is: ${name}\n`);
-  process.stdout.write('This important software is now closing\n');  
-  process.exit(0);
+  
+  // If running in terminal, exit immediately
+  if (isTTY) {
+    process.exit(0);
+  }
+});
+
+process.stdin.on('end', () => {
+  // This will only be called for piped input (not for terminal with process.exit())
+  process.stdout.write('This important software is now closing\n');
 });
