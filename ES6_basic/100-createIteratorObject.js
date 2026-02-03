0f09050f2980f7fd931f0b8460d229a89930870b
markjoehayes <markjoehayes@proton.me>
2026-02-03 12:33:22 -0400

    gererator func

---
+++ ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,9 @@
+export default function createIteratorObject(report) {
+  return (function* () {
+    for (const department in report.allEmployees) {
+      for (const employee of report.allEmployees[department]) {
+        yield employee;
+      }
+    }
+  })();
+}
